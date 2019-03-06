082f974fdec248ae150270571316d08d9ea3ed41
skino0 <skinogigo@gmail.com>
2019-03-06 14:41:04 +0200

    several changes bcz nav bug with classes

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,8 @@
 //On Click change styles
 $(".navitem").click(function(){
-    $(".navitem:first-child").attr('class','navitem active1');
-    $(".navitem:nth-child(2)").attr('class','navitem active2');
-    $(".navitem:nth-child(3)").attr('class','navitem active3');
-    
-    $(".navitem").addClass('navactive');
+    $("#aboutme").attr('id','about').attr('class','active');
+    $("#work").attr('id','wo').attr('class','active');
+    $("#contact").attr('id','co').attr('class','active');
+
 
 });
