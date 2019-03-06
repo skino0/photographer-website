10694f58a206e9d5b5f4fd2a7062b098ddf844c3
skino0 <skinogigo@gmail.com>
2019-03-06 19:51:29 +0200

    several changes

--- js/nav.js
+++ js/nav.js
@@ -6,3 +6,16 @@ $(".navitem").click(function(){
 
 
 });
+
+//Add classes when visitor scroll some pixels down
+$(document).on('scroll', function(){
+    if ($(window).scrollTop() > 100){
+        $("#aboutme").attr('id','about').attr('class','active');
+        $("#work").attr('id','wo').attr('class','active');
+        $("#contact").attr('id','co').attr('class','active');
+    }else {
+        $("#about").attr('id','aboutme').removeClass('active');
+        $("#wo").attr('id','work').removeClass('active');
+        $("#co").attr('id','contact').removeClass('active');
+    }
+ });
--- js/top.js
+++ js/top.js
@@ -1,7 +1,8 @@
 //On page refresh top
-$(document).ready(function(){
+
+/*$(document).ready(function(){
     $(this).scrollTop(0);
-});
+});*/
 
 //Go to top 
 //. . . . .
